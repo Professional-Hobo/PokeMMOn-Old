4613cb150c0eee2a1cb5544a8ce6802c6a4e5a2a
Keith Armstrong <keith1015@gmail.com>
2015-06-15 04:17:17 -0400

    Added multiple layers for tiles and made tiles compatible with PokeMap

--- public/js/Tile.js
+++
@@ -1,9 +0,0 @@
-function Tile(model, x, y) {
-    this.model = model;
-    this.x = x;
-    this.y = y;
-}
-
-Tile.prototype.render = function render() {
-    context.drawImage(this.sprite, x*16, y*16);
-}
