1c3edacd3f06b1d7a9893dab05b09785c371d0a3
Keith Armstrong <keith1015@gmail.com>
2015-06-17 01:59:45 -0500

    Added resize function

--- public/js/editor/PokeMap.js
+++ public/js/editor/PokeMap.js
@@ -3,8 +3,8 @@ var PokeMap = function() {
 
   this.scale   = 16;
   this.tile    = [0, 0];
-  this.width   = 128;
-  this.height  = 128;
+  this.width   = 10;
+  this.height  = 10;
   this.img     = new Image();
   this.img.src = "/img/editor/2_column_tileset.png";
 
@@ -19,6 +19,9 @@ var PokeMap = function() {
   // Set width and height
   this.ctx.canvas.width = this.width * this.scale;
   this.ctx.canvas.height = this.height * this.scale;
+
+  // Initialize new blank canvas
+  this.new();
 };
 
 PokeMap.prototype = {
@@ -36,16 +39,25 @@ PokeMap.prototype = {
     });
   },
 
+  // New map
+  new: function() {
+    for (var h = 0; h < this.height; h++) {
+      for (var w = 0; w < this.width; w++) {
+        this.tiles[h][w] = new Tile();
+      }
+    }
+  },
+
   // Clear the canvas
-  clearMap: function() {
+  clear: function() {
     this.ctx.clearRect(0, 0, this.width*this.scale, this.height*this.scale);
   },
 
   // Renders the canvas using the tiles array
-  renderMap: function() {
+  render: function() {
 
     // Clear the canvas first
-    this.clearMap();
+    this.clear();
 
     // Now draw all the tiles
     for (var h = 0; h < this.height; h++) {
@@ -57,8 +69,100 @@ PokeMap.prototype = {
 
   },
 
+  // Resizes the map and updates the positions
+  resize: function(direction, amount, tileObject) {
+    var self = this;
+    tileObject = tileObject || new Tile();
+
+    if (direction == "up" || direction == "down") {
+
+      // Removing rows
+      if (amount < 0) {
+        amount = Math.abs(amount);
+
+        // Make sure amount to remove is less than or equal to total rows in canvas
+        // This prevents getting into the negatives
+        amount = amount > this.height ? this.height : amount;
+
+        // Remove rows
+        for (var i = 0; i < amount; i++) {
+          direction == "up" ? this.tiles.shift() : this.tiles.pop();
+        }
+
+        // Update height of pokemap object
+        this.height -= amount;
+
+      // Adding rows
+      } else if (amount > 0) {
+        amount = Math.abs(amount);
+
+
+        for (var i = 0; i < amount; i++) {
+          // add pokemap-width number of new tiles.
+          var toAdd = Array.apply(null, Array(Math.abs(this.width))).map(function(x) {
+            return tileObject;
+          });
+
+          direction == "up" ? this.tiles.unshift(toAdd) : this.tiles.push(toAdd);
+        }
+
+        // Update height of pokemap object
+        this.height += amount;
+
+      } else {
+        console.log("What are you doing SON!?");
+      }
+
+    } else if (direction == "right" || direction == "left") {
+
+      // Removing cols
+      if (amount < 0) {
+        amount = Math.abs(amount);
+
+        // Make sure amount to remove is less than or equal to total cols in canvas
+        // This prevents getting into the negatives
+        amount = amount > this.width ? this.width : amount;
+
+        Object.keys(this.tiles).forEach(function(index) {
+          for (var i = 0; i < amount; i++) {
+            direction == "left" ? self.tiles[index].shift() : self.tiles[index].pop();
+          }
+        });
+
+        // Update width of pokemap object
+        this.width -= amount;
+
+      // Adding cols
+      } else if (amount > 0) {
+        amount = Math.abs(amount);
+
+        Object.keys(this.tiles).forEach(function(index) {
+          for (var i = 0; i < amount; i++) {
+            direction == "left" ? self.tiles[index].unshift(tileObject) : self.tiles[index].push(tileObject);
+          }
+        });
+
+        // Update width of pokemap object
+        this.width += amount;
+
+      } else {
+        console.log("What are you doing SON!?");
+      }
+
+    } else {
+      console.log("What are you doing SON!?");
+    }
+
+    // Update the attributes of the canvas tag
+    this.map.attr("width", this.width*this.scale);
+    this.map.attr("height", this.height*this.scale);
+
+    // Render new resized map
+    this.render();
+  },
+
   // Generate random map with random tiles
-  randomMap: function() {
+  random: function() {
 
     for (var h = 0; h < this.height; h++) {
       console.log("Generating random map: " + Math.ceil(h/this.height*100) + "%");
