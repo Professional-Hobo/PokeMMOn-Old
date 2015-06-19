6f7257f7161a58ce084f168100146d96ca3a69d5
Keith Armstrong <keith1015@gmail.com>
2015-06-19 00:48:12 -0500

    Added route to load ui files

--- routes/editor.js
+++ routes/editor.js
@@ -17,6 +17,7 @@ router.route('/world')
     .post(function(req, res) {
         var name = req.body.name;
         var data  = req.body.data;
+        var pattern = new RegExp("^[a-zA-Z0-9_ ]*$");
 
         if (name === undefined || data === undefined) {
             res.status(400).send({ msg: "Missing name and/or data variables!" });
@@ -24,6 +25,8 @@ router.route('/world')
             fs.exists('worlds/' + name, function(exists) {
                 if (exists) {
                     res.status(400).send({ msg: "World " + name + " already exists! Please use PUT to update world data." });
+                } else if (!pattern.test(name)) {
+                    res.status(400).send({ msg: name + " is an invalid name." });
                 } else {
                     fs.mkdir('worlds/' + name, "0755", function() {
                         fs.writeFile("worlds/" + name + '/map.json', data, function (err) {
@@ -31,7 +34,7 @@ router.route('/world')
                                 console.log(err);
                                 res.status(400).send({ msg: "An error was encountered while saving " + world + "!" });
                             } else {
-                                console.log('Saved world ' + name + '.json!');
+                                console.log('Saved world ' + name + '!');
                                 res.status(200).send({ msg: "Saved world " + name + " successfully!" });
                             }
                         });
@@ -44,7 +47,7 @@ router.route('/world')
     .get(function(req, res) {
         fs.readdir('worlds', function(err, files) {
             if (err) {
-                res.status(400).send({ msg: "An error was retrieving worlds list" });
+                res.status(400).send({ msg: "An error occured while retrieving worlds list" });
             } else {
                 res.status(200).send(files);
             }
@@ -57,7 +60,7 @@ router.route('/world/:name')
 
         fs.exists('worlds/' + name, function(exists) {
             if (exists) {
-                console.log("Loaded file " + name + ".json successfully!");
+                console.log("Loaded word " + name + " successfully!");
                 res.sendFile(process.cwd() + '/worlds/' + name + '/map.json');
             } else {
                 res.status(400).send({ msg: "World " + name + " doesn't exist!" });
@@ -81,7 +84,7 @@ router.route('/world/:name')
                             console.log(err);
                             res.status(400).send({ msg: "An error was encountered while saving world " + name + "!" });
                         } else {
-                            console.log('Updated world ' + name + '.json!');
+                            console.log('Updated world ' + name + '!');
                             res.status(200).send({ msg: "Updated world " + name + " successfully!" });
                         }
                     });
@@ -98,7 +101,7 @@ router.route('/world/:name')
                 res.status(400).send({ msg: "World " + name + " doesn't exist!" });
             } else {
                 rimraf("worlds/" + name, function () {
-                    console.log('Deleted world ' + name + '.json successfully!');
+                    console.log('Deleted world ' + name + ' successfully!');
                     res.status(200).send({ msg: "Deleted world " + name + " successfully!" });
                 });
             }
@@ -106,5 +109,29 @@ router.route('/world/:name')
 
 
     });
+
+router.get('/sets/:name', function(req, res, next) {
+
+});
+
+router.get('/sets', function(req, res, next) {
+    fs.readdir('public/img/editor/sets', function(err, files) {
+        if (err) {
+            res.status(400).send({ msg: "An error occured while retrieving sets list" });
+        } else {
+            res.status(200).send(files.map(function(file) { return file.slice(0, -4); }));
+        }
+    });
+});
+
+router.get('/uis', function(req, res, next) {
+    fs.readdir('public/js/editor/ui', function(err, files) {
+        if (err) {
+            res.status(400).send({ msg: "An error occured while retrieving ui list" });
+        } else {
+            res.status(200).send(files.map(function(ui) { return "/js/editor/ui/" + ui; }));
+        }
+    });
+});
 
 module.exports = router;
