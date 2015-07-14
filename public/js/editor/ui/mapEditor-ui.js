739b079a8b3ce39381fb2b10d0a1c5da25f1b685
Keith Armstrong <keith1015@gmail.com>
2015-07-14 07:27:52 -0500

    resolves #14

--- public/js/editor/ui/mapEditor-ui.js
+++ public/js/editor/ui/mapEditor-ui.js
@@ -1,71 +1,64 @@
 $(function() {
-    //
-    // Add map mode keyboard shortcuts
-    //
-    window.addEventListener('keydown', function(e) {
-        if (!$("input").is(":focus")) {
-            // W
-            if(e.which == 87) {
-                brush[1] -= brush[1] > 0 ? 1 : 0
-                drawTileSelector(brush[0], brush[1])
-            // S
-            } else if (e.which == 83) {
-                brush[1] += brush[1] < 997 ? 1 : 0
-                drawTileSelector(brush[0], brush[1])
-                if(brush[1] > 15){
-                    // Scroll function
-                }
-            // D
-            } else if (e.which == 68){
-                brush[0] += brush[0] < 15 ? 1 : -15
-                drawTileSelector(brush[0], brush[1])
-            // A
-            } else if (e.which == 65){
-                brush[0] -= brush[0] > 0 ? 1 : -15
-                drawTileSelector(brush[0], brush[1])
-            // Up Arrow
-            } else if (e.which == 38){
-               e.preventDefault();
-               resizeTileSelector([0, -1])
-            // Down Arrow
-            } else if (e.which == 40){
-               e.preventDefault();
-               resizeTileSelector([0, 1])
-            // Left Arrow
-            } else if (e.which == 37){
-               e.preventDefault();
-               resizeTileSelector([-1, 0])
-            // Right Arrow
-            } else if (e.which == 39){
-               e.preventDefault();
-               resizeTileSelector([1, 0])
-            }
-        }
-    });
-
-    //
-    // Update the history when clicking on 0-5
-    //
-    function clientUpdateHistory(selected){
-        var index = selected;
-        var brushx = brushHistory[index][0];
-        var brushy = brushHistory[index][1];
-        
-        brushHistory.splice(index, 1);
-        
-        brush = [brushx, brushy]
-        updateBrushHistory(brush)
+  //
+  // Add map mode keyboard shortcuts
+  //
+  window.addEventListener('keydown', function(e) {
+    if (!$("input").is(":focus")) {
+      // W
+      if (e.which == 87) {
+        tileset.selectTile(tileset.mouse.tile_x, tileset.mouse.tile_y-1);
+        // S
+      } else if (e.which == 83) {
+        tileset.selectTile(tileset.mouse.tile_x, tileset.mouse.tile_y+1);
+        // D
+      } else if (e.which == 68){
+        tileset.selectTile(tileset.mouse.tile_x+1, tileset.mouse.tile_y);
+        // A
+      } else if (e.which == 65){
+        tileset.selectTile(tileset.mouse.tile_x-1, tileset.mouse.tile_y);
+        // Up Arrow
+      } else if (e.which == 38){
+        e.preventDefault();
+        resizeTileSelector([0, -1])
+        // Down Arrow
+      } else if (e.which == 40){
+        e.preventDefault();
+        resizeTileSelector([0, 1])
+        // Left Arrow
+      } else if (e.which == 37){
+        e.preventDefault();
+        resizeTileSelector([-1, 0])
+        // Right Arrow
+      } else if (e.which == 39){
+        e.preventDefault();
+        resizeTileSelector([1, 0])
+      }
     }
+  });
 
-    $('#history li').click(function(){
-        clientUpdateHistory(this.dataset.index)
-    });
+  //
+  // Update the history when clicking on 0-5
+  //
+  function clientUpdateHistory(selected){
+    var index = selected;
+    var brushx = brushHistory[index][0];
+    var brushy = brushHistory[index][1];
 
-    // Update tileset on tileset selector change
-    $("#tilesets").change(function() {
-        // Scroll to top
-        $('#tileset-container').scrollTop(0);
+    brushHistory.splice(index, 1);
 
-        tileset.changeTileset($("#tilesets").val());
-    });
+    brush = [brushx, brushy]
+    updateBrushHistory(brush)
+  }
+
+  $('#history li').click(function(){
+    clientUpdateHistory(this.dataset.index)
+  });
+
+  // Update tileset on tileset selector change
+  $("#tilesets").change(function() {
+    // Scroll to top
+    $('#tileset-container').scrollTop(0);
+
+    tileset.changeTileset($("#tilesets").val());
+  });
 });
