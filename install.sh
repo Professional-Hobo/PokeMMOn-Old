5b41ac1441603030cbecca811099b506ac19a2bc
Keith Armstrong <keitharm@users.noreply.github.com>
2015-09-06 13:23:44 -0700

    Installation script
    This script will install the dependencies that PokeMMOn requires as well as configure the database with a user and db.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+NODE_VERSION=0.12.7
+echo "Elevating permissions to root"
+sudo echo "Permissions elevated to root"
+sudo apt-get install postgresql redis-server build-essential openssl libssl-dev curl libtool git tmux;
+wget http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz;
+tar xfv node-v$NODE_VERSION.tar.gz;
+cd node-v$NODE_VERSION;
+./configure;
+make;
+sudo make install;
+cd ..;
+rm -rf node-v$NODE_VERSION;
+git clone https://github.com/keitharm/PokeMMOn;
+sudo su - postgres -c "psql -c \"CREATE USER pokemmon WITH PASSWORD 'pokemmon'\"";
+sudo su - postgres -c "psql -c \"CREATE DATABASE pokemmon\"";
+sudo su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE pokemmon to pokemmon\"";
+cd PokeMMOn;
+./setup.sh
+./run
