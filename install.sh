cb6349414f5b17e6e563996c03128602c0668725
Keith Armstrong <keith1015@gmail.com>
2015-09-07 03:40:52 +0000

    Removed old git clone code

--- install.sh
+++ install.sh
@@ -12,10 +12,8 @@ make;
 sudo make install;
 cd ..;
 rm -rf node-v$NODE_VERSION;
-git clone https://github.com/keitharm/PokeMMOn;
 sudo su - postgres -c "psql -c \"CREATE USER pokemmon WITH PASSWORD 'pokemmon'\"";
 sudo su - postgres -c "psql -c \"CREATE DATABASE pokemmon\"";
 sudo su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE pokemmon to pokemmon\"";
-cd PokeMMOn;
 ./setup.sh
 ./run
