122d04484f17e24d06e38cb3207c11dda78d42cb
Keith Armstrong <keith1015@gmail.com>
2015-12-30 21:07:55 -0600

    Updated installation script to use latest version of node

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-NODE_VERSION=4.2.4
 echo "Elevating permissions to root"
 sudo echo "Permissions elevated to root"
 sudo apt-get update
-sudo apt-get install redis-server build-essential openssl libssl-dev curl libtool git tmux mongodb libkrb5-dev -y;
-wget http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz;
-tar xfv node-v$NODE_VERSION.tar.gz;
-cd node-v$NODE_VERSION;
-./configure;
-make;
-sudo make install;
-cd ..;
-rm -r node-v$NODE_VERSION;
-rm node-v$NODE_VERSION.tar.gz;
+sudo apt-get install redis-server build-essential openssl libssl-dev curl libtool git tmux mongodb libkrb5-dev python curl -y;
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install --yes nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 mongo < mongoSetup.txt
 rm mongoSetup.txt
 ./setup.sh
